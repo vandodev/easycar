ae6da74939b992a729283200332ba66aecc250a2
vandodev <vandoaparecido@hotmail.com>
2025-02-24 15:35:44 -0300

    :car: :taxi: :oncoming_taxi: Estilizando tela home

---
+++ mobile/src/screens/home/home.jsx
@@ -0,0 +1,29 @@
+import { Alert, Image, ImageBackground, Text, TouchableOpacity } from "react-native";
+import icons from "../../constants/icons.js";
+import { styles } from "./home.style.js";
+
+function Home(props) {
+   
+    return <ImageBackground source={icons.bg} resizeMode="cover"
+        style={styles.bg}>
+
+        <Image source={icons.logo} style={styles.logo} />
+
+        <TouchableOpacity style={styles.btn} >
+            <Image source={icons.passenger}
+                style={styles.img} />
+            <Text style={styles.title}>Passageiro</Text>
+            <Text style={styles.text}>Encontre uma carona para você</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btn}>
+            <Image source={icons.driver}
+                style={styles.img} />
+            <Text style={styles.title}>Motorista</Text>
+            <Text style={styles.text}>Ofereça carona em seu carro</Text>
+        </TouchableOpacity>
+
+    </ImageBackground>
+}
+
+export default Home;
