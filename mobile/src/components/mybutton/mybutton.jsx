06c685112281e126502f37da555bb475acae1bfe
vandodev <vandoaparecido@hotmail.com>
2025-02-24 16:37:23 -0300

    :car: :taxi: :oncoming_taxi: Criando componente botão

---
+++ mobile/src/components/mybutton/mybutton.jsx
@@ -0,0 +1,14 @@
+import { Text, TouchableOpacity } from "react-native";
+import { styles } from "./mybutton.style.js";
+
+function MyButton(props) {
+    return <TouchableOpacity
+        style={props.theme == "red" ? styles.btnRed : styles.btnYellow}>
+        <Text
+            style={props.theme == "red" ? styles.textLight : styles.textDark}>
+            {props.text}
+        </Text>
+    </TouchableOpacity>
+}
+
+export default MyButton;
--- mobile/src/screens/passenger/passenger.jsx
+++ mobile/src/screens/passenger/passenger.jsx
@@ -1,10 +1,12 @@
 import { Text,View } from "react-native";
 import { styles } from "./passenger.style.js";
+import MyButton from "../../components/mybutton/mybutton.jsx";
 
-function Passenger() {
+function Passenger(props) {
 
     return <View style={styles.container}>
        <Text>Passageiro</Text>
+       <MyButton text="Avançar" />
     </View>
 }
 
