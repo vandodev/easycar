97361e485ce6f160fefee117984aef378b960354
vandodev <vandoaparecido@hotmail.com>
2025-02-25 08:09:27 -0300

    :car: :taxi: :oncoming_taxi: Estilizando área de informação da carona

--- mobile/src/screens/passenger/passenger.jsx
+++ mobile/src/screens/passenger/passenger.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Text,View } from "react-native";
+import { Text, TextInput, View } from "react-native";
 import { styles } from "./passenger.style.js";
 import MyButton from "../../components/mybutton/mybutton.jsx";
 import MapView, { Marker, PROVIDER_DEFAULT } from "react-native-maps";
@@ -33,7 +33,27 @@ function Passenger(props) {
             />
 
         </MapView>
-        <MyButton />
+        <View style={styles.footer}>
+            <View style={styles.footerText}>
+                <Text>Encontre a sua carona</Text>
+            </View>
+
+            <View style={styles.footerFields}>
+                <Text>Origem</Text>
+                <TextInput style={styles.input} />
+            </View>
+
+            <View style={styles.footerFields}>
+                <Text>Destino</Text>
+                <TextInput style={styles.input} />
+            </View>
+
+            <View style={styles.footerFields}>
+                <Text>Motorista</Text>
+                <TextInput style={styles.input} />
+            </View>
+        </View>
+        <MyButton text="CONFIRMAR" theme="red" />
     </View>
 }
 
