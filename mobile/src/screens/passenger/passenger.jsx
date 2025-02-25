5cb5c5ecf5b6c176be9ae6a66724ef840650854c
vandodev <vandoaparecido@hotmail.com>
2025-02-25 07:07:41 -0300

    :car: :taxi: :oncoming_taxi: Mostrando mapa na tela

--- mobile/src/screens/passenger/passenger.jsx
+++ mobile/src/screens/passenger/passenger.jsx
@@ -1,12 +1,21 @@
 import { Text,View } from "react-native";
 import { styles } from "./passenger.style.js";
 import MyButton from "../../components/mybutton/mybutton.jsx";
+import MapView, { Marker, PROVIDER_DEFAULT } from "react-native-maps";
 
 function Passenger(props) {
 
     return <View style={styles.container}>
-       <Text>Passageiro</Text>
-       <MyButton text="Avançar" />
+          <MapView style={styles.map}
+            provider={PROVIDER_DEFAULT}
+            initialRegion={{
+                latitude: -23.561747,
+                longitude: -46.656244,
+                latitudeDelta: 0.004,
+                longitudeDelta: 0.004
+            }}
+        ></MapView>
+        <MyButton />
     </View>
 }
 
