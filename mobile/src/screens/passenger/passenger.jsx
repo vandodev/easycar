fb901203fe5fd2a27e724f28bc68cefa76d18e07
vandodev <vandoaparecido@hotmail.com>
2025-02-26 08:34:11 -0300

    :car: :taxi: :oncoming_taxi: Pedindo permissão para acessar localização

--- mobile/src/screens/passenger/passenger.jsx
+++ mobile/src/screens/passenger/passenger.jsx
@@ -4,18 +4,68 @@ import { styles } from "./passenger.style.js";
 import MyButton from "../../components/mybutton/mybutton.jsx";
 import MapView, { Marker, PROVIDER_DEFAULT } from "react-native-maps";
 import icons from "../../constants/icons.js";
+import {
+    getCurrentPositionAsync,
+    requestForegroundPermissionsAsync,
+    reverseGeocodeAsync
+} from "expo-location";
 
 function Passenger(props) {
-    
+   
+    const [title, setTitle] = useState("");
     const [myLocation, setMyLocation] = useState("");
 
     async function RequestRideFromUser() {
         // Acessa dados na API... 
 
-        const response = {};
+        const response = {};      
         return response;
     }
 
+    async function LoadScreen() {
+        // buscar dados de corrida aberta na API para o usuario...
+        const response = await RequestRideFromUser();
+
+        if (!response.ride_id) {
+
+            // const location = { latitude: -23.561747, longitude: -46.656244 };
+            const location = await RequestPermissionAndGetLocation();
+
+            if (location.latitude) {
+                setMyLocation(location);
+                RequestAddressName(location.latitude, location.longitude);
+            } else {
+                Alert.alert("Não foi possível obter sua localização")
+            }
+
+        } else {           
+            setMyLocation({
+                latitude: Number(response.pickup_latitude),
+                longitude: Number(response.pickup_longitude)
+            });           
+        }
+    }
+
+    async function RequestPermissionAndGetLocation() {
+
+        const { granted } = await requestForegroundPermissionsAsync();
+
+        if (granted) {
+            const currentPosition = await getCurrentPositionAsync();
+
+            if (currentPosition.coords)
+                return currentPosition.coords;
+            else
+                return {};
+        } else {
+            return {};
+        }
+    }
+
+
+    useEffect(() => {
+        LoadScreen();
+    }, []);
 
     return <View style={styles.container}>
         {myLocation.latitude ? <>
