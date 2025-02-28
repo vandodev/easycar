98bd1cd8ddda3954c720017aa0ae8ed07a4d917b
vandodev <vandoaparecido@hotmail.com>
2025-02-28 09:11:11 -0300

    :car: :taxi: :oncoming_taxi: Tela de motorista exibindo dados da carona

--- mobile/src/screens/ride-detail/ride-detail.jsx
+++ mobile/src/screens/ride-detail/ride-detail.jsx
@@ -1,8 +1,8 @@
+import { useState, useEffect } from "react";
 import { Text, TextInput, View } from "react-native";
 import MyButton from "../../components/mybutton/mybutton.jsx";
 import MapView, { Marker, PROVIDER_DEFAULT } from "react-native-maps";
 import { styles } from "./ride-detail.style.js";
-import { useState } from "react";
 import icons from "../../constants/icons.js";
 
 
@@ -10,29 +10,63 @@ function RideDetail(props) {
 
     const rideId = props.route.params.rideId;
     const userId = props.route.params.userId;
+    const [title, setTitle] = useState("");
+    const [ride, setRide] = useState({});
 
     const [myLocation, setMyLocation] = useState({
         latitude: 20,
         longitude: 20
     });
 
+    async function RequestRideDetail() {
+
+        // Acessa dados na API... 
+        
+        const response = {
+            ride_id: 1,
+            passenger_user_id: 1,
+            passenger_name: "Evandro Oliveira",
+            passenger_phone: "(11) 99999-9999",
+            pickup_address: "Praça Charles Miller - Pacaembu",
+            pickup_date: "2025-02-19",
+            pickup_latitude: "-23.543132",
+            pickup_longitude: "-46.665389",
+            dropoff_address: "Shopping Center Norte",
+            status: "P",
+            driver_user_id: null,
+            driver_name: null,
+            driver_phone: null
+        }      
+
+       
+        if (response.passenger_name) {
+            setTitle(response.passenger_name + " - " + response.passenger_phone);
+            setRide(response);
+        }
+
+    }
+
+    useEffect(() => {
+        RequestRideDetail();
+    }, []);
+
 
     return <View style={styles.container}>
         <MapView style={styles.map}
             provider={PROVIDER_DEFAULT}
             initialRegion={{
-                latitude: -23.561747,
-                longitude: -46.656244,
+                latitude: Number(ride.pickup_latitude),
+                longitude: Number(ride.pickup_longitude),
                 latitudeDelta: 0.004,
                 longitudeDelta: 0.004
             }}
         >
             <Marker coordinate={{
-                latitude: -23.561747,
-                longitude: -46.656244
+              latitude: Number(ride.pickup_latitude),
+              longitude: Number(ride.pickup_longitude),
             }}
-                title="Evandro Oliveira"
-                description="Av. Paulista, 5687"
+                title={ride.passenger_name}
+                description={ride.pickup_address}
                 image={icons.location}
                 style={styles.marker}
             />
@@ -40,17 +74,23 @@ function RideDetail(props) {
         </MapView>
         <View style={styles.footer}>
             <View style={styles.footerText}>
-                <Text>Encontre a sua carona</Text>
+                <Text>{title}</Text>
             </View>
 
             <View style={styles.footerFields}>
                 <Text>Origem</Text>
-                <TextInput style={styles.input} />
+                <TextInput style={styles.input}
+                    value={ride.pickup_address}
+                    editable={false} 
+                />
             </View>
 
             <View style={styles.footerFields}>
                 <Text>Destino</Text>
-                <TextInput style={styles.input} />
+                <TextInput style={styles.input}
+                    value={ride.dropoff_address}
+                    editable={false}
+                />
             </View>
         </View>
         <MyButton text="ACEITAR" />
