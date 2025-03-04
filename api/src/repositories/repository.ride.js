5d34e5e650bd226cd801161de5828d196e0dfdc3
vandodev <vandoaparecido@hotmail.com>
2025-03-04 08:50:04 -0300

    :car: :taxi: :oncoming_taxi: Api - Corrigindo bug de data e hora

--- api/src/repositories/repository.ride.js
+++ api/src/repositories/repository.ride.js
@@ -43,12 +43,14 @@ async function List(passenger_user_id, pickup_date, ride_id, driver_user_id, sta
 async function Insert(passenger_user_id, pickup_address,
     pickup_latitude, pickup_longitude, dropoff_address) {
 
+    let dt = new Date().toISOString("pt-BR", { timeZone: "America/Sao_Paulo" }).substring(0, 10);
+
     let sql = `insert into rides(passenger_user_id, pickup_address,
         pickup_latitude, pickup_longitude, dropoff_address, pickup_date, status)
-        values(?, ?, ?, ?, ?, CURRENT_DATE, 'P') returning ride_id`;
+        values(?, ?, ?, ?, ?, ?, 'P') returning ride_id`;
 
     const ride = await execute(sql, [passenger_user_id, pickup_address,
-        pickup_latitude, pickup_longitude, dropoff_address]);
+        pickup_latitude, pickup_longitude, dropoff_address, dt]);
 
     return ride[0];
 }
